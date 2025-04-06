7eb83c496687b4c3ca87bfdc6b8de378eec28c3d
Richardlws <liu.bing66066@hotmail.com>
2025-04-06 09:12:18 +0800

    CREATE A PI GAME WINDOW

---
+++ 2048/main.py
@@ -0,0 +1,27 @@
+import pygame
+import random
+import math
+
+pygame.init()
+
+FPS = 60
+
+WIDTH, HEIGHT = 800, 800
+ROWS = 4
+COLS = 4
+
+RECT_HEIGHT = HEIGHT // ROWS
+RECT_WIDTH = WIDTH // COLS
+
+OUTLINE_COLR = (187, 173, 160)
+OUTLINE_THICKNESS = 10
+BACKGROUND_COLOR = (205, 192, 180)
+FONT_COLOR = (119, 110, 101)
+
+FONT = pygame.font.SysFont("comic", 60, bold=True)
+MOVE_VEL = 20
+
+WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("2048")
+
+
