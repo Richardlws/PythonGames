31849f9801da9ef89d78273ad1184a38e84a542c
Richardlws <liu.bing66066@hotmail.com>
2025-04-06 10:21:43 +0800

    DRAWING

--- 2048/main.py
+++ 2048/main.py
@@ -13,7 +13,7 @@ COLS = 4
 RECT_HEIGHT = HEIGHT // ROWS
 RECT_WIDTH = WIDTH // COLS
 
-OUTLINE_COLR = (187, 173, 160)
+OUTLINE_COLOR = (187, 173, 160)
 OUTLINE_THICKNESS = 10
 BACKGROUND_COLOR = (205, 192, 180)
 FONT_COLOR = (119, 110, 101)
@@ -24,16 +24,38 @@ MOVE_VEL = 20
 WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("2048")
 
+
+def draw_grid(window):
+    for row in range(1, ROWS):
+        y = row * RECT_HEIGHT
+        pygame.draw.line(window, OUTLINE_COLOR, (0, y), (WIDTH, y), OUTLINE_THICKNESS)
+    for col in range(1, COLS):
+        x = col * RECT_WIDTH
+        pygame.draw.line(window, OUTLINE_COLOR, (x, 0), (x, HEIGHT), OUTLINE_THICKNESS)
+    pygame.draw.rect(window, OUTLINE_COLOR, (0, 0, WIDTH, HEIGHT), OUTLINE_THICKNESS)
+
+
+def draw(window):
+    window.fill(BACKGROUND_COLOR)
+
+    draw_grid(window)
+
+    pygame.display.update()
+
+
 def main(window):
     clock = pygame.time.Clock()
     run = True
 
     while run:
         clock.tick(FPS)
+        # pygame.display.update()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
                 break
+
+        draw(window)
 
     pygame.quit()
 
